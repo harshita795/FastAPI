9628f056070389f61615ca4b8a9d729796a39edf
harshita795 <yadavharshita261303@gmail.com>
2026-06-25 12:46:42 +0530

    feat: add get view endpoint

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 from fastapi import FastAPI
+import json
 
 app = FastAPI()
 
+def load_data():
+  with open("patients.json", "r") as f:
+    data = json.load(f)
+
+  return data
+
 @app.get("/")
 def hello():
-  return {"message":  "Hello World"}
+  return {"message": "Patient Management System API"}
 
 @app.get("/about")
 def about():
-  return {"message" : "Hi, I am Harshita, an Integration Engineer, exploring AI engineering."}
+  return {"message" : "A fully functional API to manage your patient records."}
+
+@app.get("/view")
+def view():
+  data = load_data()
+  return data
